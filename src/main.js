98c1e7de67fb9ed6ca346dc4a08f50abe29e6d12
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2025-10-03 14:41:26 +0300

    add Header and Hero

--- src/main.js
+++ src/main.js
@@ -1,3 +1,55 @@
-import './styles/styles.scss';
+import "./styles/styles.scss";
 
-console.log('Vite работает!');
+const blurHeader = () => {
+  const header = document.body.querySelector("[data-js-header]");
+
+  if (header) {
+    header.classList.remove("is-blured");
+
+    window.addEventListener("scroll", () => {
+      if (scrollY > 0 || !header.classList.contains("is-blured")) {
+        header.classList.add("is-blured");
+      }
+      if (scrollY === 0) {
+        header.classList.remove("is-blured");
+      }
+    });
+  }
+};
+
+const useMobileMenu = () => {
+  const header = document.body.querySelector("[data-js-header]");
+  const navigation = header.querySelector("[data-js-navigation]");
+  const button = navigation.querySelector("[data-js-navigation-button]");
+
+  let isMenuOpened = false;
+
+  const openMenu = () => {
+    header.classList.add("is-menu-opened");
+    navigation.classList.add("is-menu-opened");
+    isMenuOpened = true;
+  };
+
+  const closeMenu = () => {
+    header.classList.remove("is-menu-opened");
+    navigation.classList.remove("is-menu-opened");
+    isMenuOpened = false;
+  };
+
+  button?.addEventListener("click", () => {
+    if (isMenuOpened) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    if (isMenuOpened) {
+      closeMenu();
+    }
+  });
+};
+
+blurHeader();
+useMobileMenu();
